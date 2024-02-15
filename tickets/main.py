a78228dcf78468cfe7cf73b2987bb2d0caba44d7
Vito Genovese <genovese.work@protonmail.com>
2024-02-15 21:03:03 +0100

    FEAT update

--- tickets/main.py
+++ tickets/main.py
@@ -15,11 +15,12 @@ def print_center(word: str, separator:str = " "):
 def print_full(word: str, separator:str = " "):
     print(word.center(width,separator))
 def menu():
-    print_full("#", "#")
+    print_full("_", "_")
     print_center("1. Crear ticket")
     print_center("2. Eliminar ticket")
+    print_center("3. Actualizar ticket")
     print_center("Q. Salir")
-    print_full("#", "#")
+    print_full("_", "_")
 
 
 
@@ -54,13 +55,36 @@ while user.lower() != "q":
             description = description[:15] if len(description) > 15 else description
             print_center(f"{i + 1}. {email} | {description[:15]}...")
         print_full("-", "-")
-        user = int(input(": ")) - 1
-        ticket_id = tickets[user][0]
+        try:
+            user = int(input(": ")) - 1
+        except ValueError:
+            print(f"Debe indicar un número del 1 al {len(tickets)}")
+            input(": ")
+            user = ""
+            continue
+        try:
+            ticket_id = tickets[user][0]
+        except IndexError:
+            print(f"Debe indicar un número del 1 al {len(tickets)}")
+            input(": ")
+            user = ""
+            continue
         ticket_to_delete = list(cur.execute(f"SELECT * FROM tickets WHERE id = '{ticket_id}';"))
         print_center(f"¿Desea eliminar el ticket con identificador: {ticket_id}?(Y/N)")
         user = input(": ")
         if user.upper() == "Y":
             cur.execute(f"DELETE FROM TICKETS WHERE id = ?;", (ticket_id,))
-        
-        
         user = input(": ")
+    
+    elif user == "3": # Opción UPDATE
+        con.close()
+        con = sqlite3.connect("tickets.db")
+        def make_dicts(cursor, row):
+            return dict((cursor.description[idx][0], value)
+                for idx, value in enumerate(row))
+
+        con.row_factory = make_dicts
+        cur = con.cursor()
+        print(next(cur.execute("SELECT * FROM tickets LIMIT 1")))
+        con.commit()
+        input(": ")
