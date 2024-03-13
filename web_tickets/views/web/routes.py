6f2a2608bf4cc3bdbbb0903f0e56f68285b0572e
Vito Genovese <genovese.work@protonmail.com>
2024-03-13 19:26:52 +0100

    a

--- web_tickets/views/web/routes.py
+++ web_tickets/views/web/routes.py
@@ -7,6 +7,5 @@ web = Blueprint("web", __name__)
 def t_home():
     # res = req.get("https://pokeapi.co/api/v2/pokemon/charmander").json()
     # print(res["sprites"]["back_default"])
-    res = req.get("http://localhost:3000/tickets").json()
-    print(res)
-    return render_template("index.html")
+    data = req.get("http://localhost:3000/tickets").json()
+    return render_template("index.html", tickets=data)
