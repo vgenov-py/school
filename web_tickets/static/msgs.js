49baae603b08ffe8d3eb347f6f5f70bfa93ccc29
Vito Genovese <genovese.work@protonmail.com>
2024-04-09 19:53:05 +0200

    js init

---
+++ web_tickets/static/msgs.js
@@ -0,0 +1,6 @@
+function delete_msg() {
+    const msg = document.querySelector("#msg");
+    msg.style.display = "none";
+};
+
+setTimeout(delete_msg, 2500);
--- web_tickets/views/web/routes.py
+++ web_tickets/views/web/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash, redirect, session
 import requests as req
 
 web = Blueprint("web", __name__)
@@ -25,10 +25,17 @@ def t_base(ticket_id):
 
 @web.route("/ticket/update/<ticket_id>", methods=["GET", "POST"])
 def t_update(ticket_id):
-    params = dict(request.form)
-    params["ticket_id"] = ticket_id # {id:1, description: "nueva descripción"}
-    res_api = req.patch(f"{base_api_url}/tickets", params=params)
-    if res_api.status_code == 200:
-        res_api = res_api.json()
-        print("Ticket actualizado!")
+    session["id"] = "ID DEL USUARIO"
+    if request.method == "POST":
+        params = dict(request.form)
+        params["ticket_id"] = ticket_id # {id:1, description: "nueva descripción"}
+        res_api = req.patch(f"{base_api_url}/tickets", params=params)
+        if res_api.status_code == 200:
+            res_api = res_api.json()
+            if res_api.get("success"): # res_api["success"]
+                flash("Ticket actualizado correctamente!")
+                return redirect("/")
+            else:
+                flash("Algo ha ido mal!")
+            print("Ticket actualizado!")
     return render_template("update.html")
